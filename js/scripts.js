74bcd831dc27dd3585ddcf539e727a22ee107183
Dan Watson <danwatson00@yahoo.com>
2018-01-11 13:37:20 -0600

    javascript updated w code

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,60 @@
 
 var karate = {
-    name: "Martial Arts"
-    description:
-    price: $199
-    URL: /images/karate-01.jpg
+    name: "Martial Arts",
+    description:"",
+    price: "$199.00",
+    URL: "/images/karate-01.jpg"
 }
 
 var art = {
-    name: "Painting"
-    description:
-    price: $199
-    URL: /images/art-01.jpg
+    name: "Painting",
+    description:"",
+    price: "$199",
+    URL: "/images/art-01.jpg"
 }
 
 var canoeing = {
-    name: "Canoeing"
-    description:
-    price: $199
-    URL: /images/canoe-01.jpg
+    name: "Canoeing",
+    description:"",
+    price: "$199",
+    URL: "/images/canoe-01.jpg"
 }
 
 var dance = {
-    name: "Dance"
-    description:
-    price: $199
-    URL: /images/karate-01.jpg
+    name: "Dance",
+    description:"",
+    price: "$199",
+    URL: "/images/karate-01.jpg"
 }
 
 var kickball = {
-    name: "Kickball"
-    description:
-    price: $199
-    URL: /images/kickball-01.jpg
+    name: "Kickball",
+    description:"",
+    price: "$199",
+    URL: "/images/kickball-01.jpg"
 }
 
 var woodworking = {
-    name: "Woodworking"
-    description:
-    price: $199
-    URL: /images/wood-01.jpg
+    name: "Woodworking",
+    description:"",
+    price: "$199",
+    URL: "/images/wood-01.jpg"
 }
 
 var ropes = {
-    name: "Ropes Course"
-    description:
-    price: $49
-    URL: /images/ropes01.jpg
+    name: "Ropes Course",
+    description:"",
+    price: "$49",
+    URL: "/images/ropes01.jpg"
 }
 
 var stem = {
-    name: "S.T.E.M."
-    description:
-    price: $99
-    URL: /images/stem-01.jpg
+    name: "S.T.E.M.",
+    description:"",
+    price: "$99",
+    URL: "/images/stem-01.jpg"
 }
 
-
 var products = [karate, woodworking, art, dance, kickball, canoeing, stem, ropes]
+
+var desc = '<p>name<br> + description<br> + price</p>'
